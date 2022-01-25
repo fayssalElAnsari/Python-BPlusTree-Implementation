bfb521ec358e4d50b9fbcb8d7abecba7d5f9efcf
El Ansari Fayssal <elansari@a10p25.fil.univ-lille1.fr>
2022-01-25 14:45:31 +0100

    starting with simple forms of trees to end with a BTree

--- BTree.py
+++ BTree.py
@@ -1,40 +1,42 @@
 
 '''
-cette classe contiendra la definition d'un noeud 
-un noeud peut etre une feuille, on definit si c'est une 
-feuille par une variable isLeaf
-the children is a list of nodes that are beneath this tree
+In computer science, a B-tree is a self-balancing tree data structure
+that maintains sorted data and allows searches,
+sequential access, insertions, and deletions in logarithmic time:
+1. Every node has at most m children.
+2. Every non-leaf node (except root) has at least ⌈m/2⌉ child nodes.
+3. The root has at least two children if it is not a leaf node.
+4. A non-leaf node with (k) children contains (k-1) keys.
+5. All leaves appear in the same level and carry no information.
 ...
 repr
 iter
 next
 '''
-class BTree:
-    
-    class Node:
-        def __init__(self, l, u, e):
-            self.children = []
-            self.elements = [e]
-            self.isLeaf = True
-
-
-    def __init__(self, l, u):
-        self.elements = []
+class Node:
+    def __init__(self, l, u, e):
         self.children = []
-        self.L = l
-        self.U = u
-        self.isLeaf = False
+        self.elements = [e]
+        self.isLeaf = True
 
 
-    get getSize(self):
-        return self.
+def __init__(self, l, u):
+    self.elements = []
+    self.children = []
+    self.L = l
+    self.U = u
+    self.isLeaf = False
 
 
-    def insert(self, e):
-        self.elements.append(e)
+get getSize(self):
+    return self.
 
 
-    def delete(self, e):
-        self.elements.remove(e)
+def insert(self, e):
+    self.elements.append(e)
+
+
+def delete(self, e):
+    self.elements.remove(e)
 
 
---
+++ N-ary-Tree.py
@@ -0,0 +1,23 @@
+
+
+'''
+The definition of an N-Ary Tree is:
+
+'''
+def class Node:
+    def __init__(self, value, children=None):
+        self.value = value
+        if children == None:
+            self.children = []
+        else:
+            self.children = children
+
+    def insert:
+        pass
+    
+    def delete:
+        pass
+
+    
+
+    
