17245569ab449c455ee5efacb0417a9ef023fcf0
El Ansari Fayssal <elansari@a15p9.fil.univ-lille1.fr>
2022-02-01 16:28:04 +0100

    checkpoint

--- BPNode.py
+++ BPNode.py
@@ -1,4 +1,8 @@
 
+# TODO: insert function
+# TODO: find function
+# TODO: delete function
+# TODO: show tree functionS
 class BPNode:
     '''
     A B+ tree is an m-ary tree with a variable but often large number of children per node. 
@@ -33,17 +37,17 @@ class BPNode:
         self.elements = e
         self.keys = []
 
-        self.isRoot = isRoot
-        self.isLeaf = False
+        self.isRoot = isRoot # by default not a root
+        self.isLeaf = True #every leaf is a leaf node at creation
 
         self.leftSibling = None
         self.rightSibling = None
-        self.root = None
+        self.parent = None
 
         self.insert(e)
 
 
-    def insert(self, e):
+    def insert(self, new_element):
         '''
         ## algo:
         1. we check to see if the number of child nodes is surpassed
@@ -51,7 +55,7 @@ class BPNode:
 
         3. if true: 
             split the current node into two nodes
-            (if the number of elements is odd then the rightSibling will have and odd number)
+            (if the number of elements is odd then the rightSibling will have an odd number)
         4. see if leftSibling == None
             if true:
                 if rightSibling == None
@@ -59,17 +63,67 @@ class BPNode:
                 else: rightSibling.insert(e) #recursively
             else:
                 leftSibling.insert(e)
-
         ## we will insert only one element at a time for the sake of simplicity
+        
         '''
         if (len(self.children) >= self.m) or (len(self.elements) >= self.l):
-            self.split(e)
-        elif e == []:# inesrt the value in children and update the root's keys if necessary
+            if(self.leftSibling == None):
+                if(self.rightSibling == None):
+                    self.split(new_element)
+            #     else: self.rightSibling.insert(e)
+            # else: self.leftSibling.insert(e)
+            
+        elif new_element == []:
             pass
-        else:    
-            self.elements.append(e)# we need to organize this list after insertion
-            if self.root:
-                self.root.update_keys()
+        else:    # inesrt the value in children and update the root's keys if necessary
+            self.elements.append(new_element)
+            self.elements.sort(reverse=False)
+            # print(self.elements)
+
+            if self.parent:
+                self.parent.update_keys()
+
+    def split(self, new_element):
+        '''
+        splits the current node into two and updates the keys
+        THIS node will become a left sibling or stay the root node of this tree?? 
+            > will choose stay root
+        how to go from the root node to the children???
+            > using the keys?
+        when splitting there are two cases:
+            1. if the root to split is the root node it will stay the root node
+            2. if not the root node it will become the left node 
+        '''
+        print("splitting...")
+        leftNode = BPNode(self.m, self.l)
+        rightNode = BPNode(self.m, self.l)
+        
+        leftNode.parent = self
+        rightNode.parent = self
+
+        leftNode.rightSibling = rightNode
+        rightNode.leftSibling = leftNode
+        
+        for element in self.elements[:(self.l)//2] :
+            leftNode.insert(element)
+
+        for element in self.elements[(self.l)//2:] :
+            rightNode.insert(element)
+
+        rightNode.insert(new_element)
+
+        self.children.append(leftNode)
+        self.children.append(rightNode)
+
+        print(leftNode)
+        print(rightNode)
+
+        # turn into an inner node
+        self.isLeaf = False
+        self.elements = []
+        self.keys.append(rightNode.elements[0])
+
+        self.update_keys()
 
 
     def find(self, e):
@@ -92,21 +146,6 @@ class BPNode:
             for child in children:
                 if type(child) is BPNode:
                     child.delete(e)
-        
-    def split(self, e):
-        '''
-        splits the current node into two and updates the keys
-        '''
-        leftNode = BPNode(self.l, self.m, self.elements[:(self.m)//2])
-        rightNode = BPNode(self.l, self.m, self.elements[(self.m)//2:])
-        rootNode = BPNode(self.l, self.m)
-
-        leftNode.rightSibling = rightNode
-        rightNode.leftSibling = leftNode
-
-        rightNode.insert(e)
-        rootNode.keys.append(rightNode.elements[0])
-        self.update_keys()
 
     def update_keys(self):
         '''
@@ -131,11 +170,16 @@ class BPNode:
 
 
     def __repr__(self):
-        return "Node :{keys=[" + str(self.keys) + "] ; elements=[" + str(self.elements) + "]; children=["+str(self.children) +"]; "
+        return "Node :{keys=" + str(self.keys) + " ; elements=" + str(self.elements) + "; children="+str(self.children) +"; }"
         
 
     def __str__(self):
-        return "Node :{keys=[" + str(self.keys) + "] ; elements=[" + str(self.elements) + "]; children=["+str(self.children) +"]; "
+        return "Node :{keys=" + str(self.keys) + " ; elements=" + str(self.elements) + "; children="+str(self.children) +"; }"
+
+    def print_tree(self):
+        # for child in self.children:
+        #     child.print_tree()
+        print(self)
 
 def test1():
     BPTree = BPNode(3, 4, [], True)
@@ -143,8 +187,12 @@ def test1():
     BPTree.insert(18)
     BPTree.insert(22)
     BPTree.insert(28)
-    print(BPTree)
-    BPTree.insert(25)
+    BPTree.print_tree()
+
+    BPTree.insert(25) # 5 elements in a leaf node that can only contain (L=4) elements
+    # BPTree.insert(41)
+    # BPTree.insert(23)
+    BPTree.print_tree()
 
 
 def main():
