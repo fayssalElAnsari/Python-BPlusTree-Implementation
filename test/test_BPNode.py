3bd725625c08b26a61bdee1fd9ad7a0dfa724a0d
fayssalElAnsari <fayssal.el.ansari@gmail.com>
2022-03-17 10:49:31 +0100

    housekeeping
    unit tests boilerplate code, CI

---
+++ test/test_BPNode.py
@@ -0,0 +1,36 @@
+import unittest
+
+class TestBPNode(unittest.TestCase):
+    def test_isRoot(self):
+        pass
+
+    def test_isLeaf(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+    def test_split(self):
+        pass
+
+    def test_find(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_update_keys(self):
+        pass
+
+    def test_balance(self):
+        pass
+
+    def test_print_tree(self):
+        pass
+
+    def test_visualize_tree(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_BPTree.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestBPtree(unittest.TestCase):
+
+
+    def test_find(self):
+        pass   
+
+
+if __name__ == "__main__":
+    unittest.main()
