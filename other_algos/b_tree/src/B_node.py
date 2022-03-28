783cbfddac97a44b4851571b584bab40e08ffe12
fayssalElAnsari <fayssal.el.ansari@gmail.com>
2022-03-28 18:26:48 +0200

    b_node.py search_element()

---
+++ other_algos/b_tree/src/B_node.py
@@ -0,0 +1,33 @@
+class B_node:
+    def main():
+        print("B_tree")
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.keys = []
+        self.children = []
+        
+
+    def search_element(self, element):
+        if element in self.keys:
+            return true
+        else:
+            if self.is_leaf():
+                return false
+            else:
+                for i in range(len(self.keys) - 1):
+                    if element < keys[i]:
+                        self.children[i].search_element(element)
+                        break
+                self.children[-1].search_element(element)
+        
+
+    def search_node(self, element):
+
+        pass
+
+    def is_leaf(self):
+        return true if self.children == [] else false
+
+    if __name__ == '__main__':
+        main()
---
+++ other_algos/b_tree/test/test_b_tree.py
@@ -0,0 +1,33 @@
+import unittest
+
+class test_b_tree(unittest.TestCase):
+    def test_all_keys_increasing_order(self):
+        pass
+
+    def test_all_leafs_have_no_children(self):
+        pass
+
+    def test_all_internal_nodes_have_at_most_n_minus_one_keys(self):
+        pass
+
+    def test_all_internal_nodes_have_at_most_n_children(self):
+        pass
+
+    def test_all_internal_nodes_have_at_least_n_over_two_children(self):
+        pass
+
+    def all_leaves_have_same_depth(self):
+        pass
+
+    def root_node_have_minimum_of_one_key(self):
+        pass
+
+    def all_non_leaf_nodes_have_at_least_two_children(self):
+        pass
+    
+    def non_leaf_node_with_k_minus_one_keys_contains_k_children(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
